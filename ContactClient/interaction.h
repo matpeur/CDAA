d6a86da421bfb272aba980619d111138f07de858
matpeur <78799819+matpeur@users.noreply.github.com>
2021-10-06 17:18:44 +0200

    gg

--- ContactClient/interaction.h
+++ ContactClient/interaction.h
@@ -1,15 +1,25 @@
 #ifndef INTERACTION_H
 #define INTERACTION_H
 
-#include <QObject>
+#include<string>
+#include <ctime>
 
-class Interaction : public QObject
+class Interaction
 {
-    Q_OBJECT
-public:
-    explicit Interaction(QObject *parent = nullptr);
 
-signals:
+public:
+    Interaction(std::string contenu);
+
+    std::string getContenu() const;
+    void setContenu(const std::string contenu);
+    time_t getDate() const;
+    std::string getDateToString() const;
+
+
+
+private:
+    time_t date;
+    std::string contenu;
 
 };
 
