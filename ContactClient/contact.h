1d169a0a980a06282571a9e8b154b1883f470b46
matpeur <78799819+matpeur@users.noreply.github.com>
2021-10-06 16:34:56 +0200

    yuu

---
+++ ContactClient/contact.h
@@ -0,0 +1,16 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+
+#include <QObject>
+
+class Contact : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Contact(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // CONTACT_H
