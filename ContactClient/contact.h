13d944163d3072cf387ace0d37cbbe651576eede
Musa753 <80329531+Musa753@users.noreply.github.com>
2021-10-09 15:07:35 +0200

    Update contact.h

--- ContactClient/contact.h
+++ ContactClient/contact.h
@@ -1,3 +1,4 @@
+
 #ifndef CONTACT_H
 #define CONTACT_H
 
@@ -10,20 +11,31 @@ class Contact : public QObject
     Q_OBJECT
 public:
     explicit Contact(QObject *parent = nullptr);
-    ajoutinterraction(interaction i);
-    removeinterracion(Interaction i);
-   std :: ostream & operator < < (std :: ostream & , const & value );
+    void addinterraction(interaction i);
+    void removeinterracion(Interaction i);
+    friend  std :: ostream & operator < < (std :: ostream & os, const Contact Conct );
+    void setNom(String n) ;
+    void setPreNom(String n) ;
+    void setentrprise(String e) ;
+    void settelephoen(String tel);
+    void setPhoto(String photo) ;
+    void setDate(date t) ;
+    String getNom() ;
+    String getPrenom();
+    String getEntreprise();
+    String gettelephone() ;
+    String getPhoto() ;
+   // date getDate();
 private:
       String nom;
       String prenom;
       String entreprise;
       String telephone;
       String Photo;
-      date d;
-      list<Interaction>;
+    //  date d;
+      list<Interaction>  Listesinterractions;
 
 signals:
-
 };
-
 #endif // CONTACT_H
+
