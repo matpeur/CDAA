e55a64c207c073b4a6c333d304b355488ffc35a6
Musa753 <80329531+Musa753@users.noreply.github.com>
2021-10-13 00:05:57 +0200

    Update contact.cpp

--- ContactClient/contact.cpp
+++ ContactClient/contact.cpp
@@ -1,5 +1,6 @@
 #include "contact.h"
-
+#include <iostream>
+using namespace std;
 Contact::Contact()
 {
 
@@ -8,7 +9,7 @@ void Contact::addinterraction(const Interaction &i);
 {
 
  Listesinterractions.push_back(i);
-
+ this->dernieredate=ths->d->tm;
 }
 
 void Contact::removeinterracion(Interaction i)
@@ -23,6 +24,7 @@ void Contact::removeinterracion(Interaction i)
        {
            found =true;
            it=Listesinterractions.erase(it);
+            this->dernieredate=this->d->getDateToString();
        }
        else
        {
@@ -41,18 +43,18 @@ void   Contact::setPreNom(std::string n){this -> prenom=n;}
 void   Contact:: setentrprise(std::string e){this ->entreprise=e;}
 void   Contact:: settelephoen(std::string tel){this ->telephone=tel;}
 void   Contact:: setPhoto(std::string photo){this->Photo=photo;}
-void   Contact:: setDate(date t){this ->d=t;}
-std::string Contact:: getNom(){return this ->nom;}
-std::string Contact::getPrenom(){ return this ->prenom;}
-std::string Contact:: getEntreprise(){return this ->entreprise;}
-std::string Contact::gettelephone(){ return this ->telephone;}
-std::string Contact::getPhoto(){ return this ->Photo;}
-date   Contact:: getDate(){return this->date ;}
+void   Contact:: setDate(date t){this->d=t;}
+string Contact:: getNom(){return this->nom;}
+string Contact::getPrenom(){ return this->prenom;}
+string Contact:: getEntreprise(){return this->entreprise;}
+string Contact::gettelephone(){ return this->telephone;}
+string Contact::getPhoto(){ return this->Photo;}
 
-friend Contact::std::ostream & operator << (std :: ostream & os, const Contact C )
+
+friend Contact::ostream & operator << (ostream & os, const Contact C )
 {
 
-os << C.getPrenom()<<",␣"<<C.getNom()<<",␣"<<C.getEntreprise()<<",␣"<<C.getPhoto()<<",␣"<<C.getDate()<<",␣"<<C.gettelephone()<<",␣"<<;
+  os << C.getPrenom()<<",␣"<<C.getNom()<<",␣"<<C.getEntreprise()<<",␣"<<C.getPhoto()<<",␣"<<C.getDate()<<",␣"<<C.gettelephone()<<",␣"<<;
  return os ;
 
 
