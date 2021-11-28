d15c4524c8118f50f45b5ee7d89a68cfd8d45942
Musa753 <80329531+Musa753@users.noreply.github.com>
2021-11-28 04:43:18 +0100

    Add files via upload

--- basededonnee.cpp
+++ basededonnee.cpp
@@ -31,73 +31,78 @@ void Basededonnee::AjoutContact(Contact P)
 {
 
 
-   if(db.open())
-   {
-    QSqlQuery query;
-    query.prepare("INSERT IN TO Conatct(nom,prenom,entreprise,mail,telephone,photo,date)" "VALUES(:A,:B,:C,:D:,:E,:F,:G)");
-    query.bindValue(":A",QString::fromStdString(P.getNom()));
-    query.bindValue(":B",QString::fromStdString(P.getPrenom()));
-    query.bindValue(":C",QString::fromStdString(P.getEntreprise()));
-    query.bindValue(":D",QString::fromStdString(P.getPhoto()));
-    query.bindValue(":E",QString::fromStdString(P.getTelephone()));
-    query.bindValue(":F",QString::fromStdString(P.getPhoto()));
-    query.bindValue(":G",QString::fromStdString(P.getDate()));
-
-    if(!query.exec())
+    if(db.open())
     {
-        qDebug()<<"erreur lors de la  requete";
+
+     QSqlQuery query;
+     query.prepare("INSERT INTO conta(nom,prenom,entreprise,mail,telephone,photo,date)" "VALUES(:A,:B,:C,:D,:E,:F,:G)");
+     query.bindValue(":A",QString::fromStdString(P.getNom()));
+     query.bindValue(":B",QString::fromStdString(P.getPrenom()));
+     query.bindValue(":C",QString::fromStdString(P.getEntreprise()));
+     query.bindValue(":D",QString::fromStdString(P.getPhoto()));
+     query.bindValue(":E",QString::fromStdString(P.getTelephone()));
+     query.bindValue(":F",QString::fromStdString(P.getPhoto()));
+     query.bindValue(":G",QString::fromStdString(P.getTelephone()));
+
+     if(!query.exec())
+     {
+         qDebug()<<"erreur lors de la  requete";
 
 
 
-    }
+     }
 
-
-  }
 
 
 
 
 }
+
+}
+
 
 void Basededonnee::SupprimeContact(Contact P)
 {
-   if(db.open())
-   {
-    QSqlQuery query;
-    query.prepare("DELETE FROM personne WHERE nom=:A and prenom=:B and entreprise=:C and mail=:D and telephone=:E and photo=:F and date=:G");
-    query.bindValue(":A",QString::fromStdString(P.getNom()));
-    query.bindValue(":B",QString::fromStdString(P.getPrenom()));
-    query.bindValue(":C",QString::fromStdString(P.getEntreprise()));
-    query.bindValue(":D",QString::fromStdString(P.getPhoto()));
-    query.bindValue(":E",QString::fromStdString(P.getTelephone()));
-    query.bindValue(":F",QString::fromStdString(P.getPhoto()));
-    query.bindValue(":G",QString::fromStdString(P.getDate()));
-
-    if(!query.exec())
+    if(db.open())
     {
-        qDebug()<<"erreur lors de la requete";
+     QSqlQuery query;
+     query.prepare("DELETE FROM conta WHERE nom=:A and prenom=:B and entreprise=:C and mail=:D and telephone=:E and photo=:F and date=:G");
+     query.bindValue(":A",QString::fromStdString(P.getNom()));
+     query.bindValue(":B",QString::fromStdString(P.getPrenom()));
+     query.bindValue(":C",QString::fromStdString(P.getEntreprise()));
+     query.bindValue(":D",QString::fromStdString(P.getPhoto()));
+     query.bindValue(":E",QString::fromStdString(P.getTelephone()));
+     query.bindValue(":F",QString::fromStdString(P.getPhoto()));
+     query.bindValue(":G",QString::fromStdString(P.getTelephone()));
+
+     if(!query.exec())
+     {
+         qDebug()<<"erreur lors de la requete";
 
 
 
-    }
-  }
+     }
+   }
+
+
+
 
 
 }
-
-
-void Basededonnee::ModifiContact(Contact C, Contact old)
+void Basededonnee::ModifiContact(Contact old, Contact C)
 {
+
+
     QSqlQuery query ;
 
-    query.prepare( "UPDATE Contact  SET nom=:a , prenom=:b , entreprise=:c,mail=:d,telephone=:e,photo:f,date=:g WHERE nom=:A and prenom=:B and entreprise=:C and mail=:D and telephone=:E and photo=:F and date=:G" ) ;
+    query.prepare( "UPDATE Conta  SET nom=:a , prenom=:b , entreprise=:c,mail=:d,telephone=:e,photo=:f,date=:g WHERE nom=:A and prenom=:B and entreprise=:C and mail=:D and telephone=:E and photo=:F and date=:G" ) ;
     query.bindValue(":A",QString::fromStdString(C.getNom()));
     query.bindValue(":B",QString::fromStdString(C.getPrenom()));
     query.bindValue(":C",QString::fromStdString(C.getEntreprise()));
     query.bindValue(":D",QString::fromStdString(C.getPhoto()));
     query.bindValue(":E",QString::fromStdString(C.getTelephone()));
     query.bindValue(":F",QString::fromStdString(C.getPhoto()));
-    query.bindValue(":G",QString::fromStdString(C.getDate()));
+    query.bindValue(":G",QString::fromStdString(C.getTelephone()));
 
     query.bindValue(":a",QString::fromStdString(old.getNom()));
     query.bindValue(":b",QString::fromStdString(old.getPrenom()));
@@ -105,13 +110,30 @@ void Basededonnee::ModifiContact(Contact C, Contact old)
     query.bindValue(":d",QString::fromStdString(old.getPhoto()));
     query.bindValue(":e",QString::fromStdString(old.getTelephone()));
     query.bindValue(":f",QString::fromStdString(old.getPhoto()));
-    query.bindValue(":g",QString::fromStdString(old.getDate()));
+    query.bindValue(":g",QString::fromStdString(old.getTelephone()));
 
     if(!query.exec())
     {
-        qDebug()<<"erreur lors de la requete";
+        qDebug()<<"erreur";
 
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
