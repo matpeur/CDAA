2c990ab05754fbb450c84f768f4305797ca41c93
matpeur <mathieu.bellegueulle@gmail.com>
2021-12-10 10:44:31 +0100

    build

---
+++ build-ContactClient-Desktop_Qt_5_12_11_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,92 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.11
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionNouveau_Contact;
+    QAction *actionNouvelle_Interaction;
+    QWidget *centralwidget;
+    QHBoxLayout *horizontalLayout;
+    QTableView *tableView;
+    QMenuBar *menubar;
+    QMenu *menuAjouter;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        actionNouveau_Contact = new QAction(MainWindow);
+        actionNouveau_Contact->setObjectName(QString::fromUtf8("actionNouveau_Contact"));
+        actionNouvelle_Interaction = new QAction(MainWindow);
+        actionNouvelle_Interaction->setObjectName(QString::fromUtf8("actionNouvelle_Interaction"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        horizontalLayout = new QHBoxLayout(centralwidget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        tableView = new QTableView(centralwidget);
+        tableView->setObjectName(QString::fromUtf8("tableView"));
+
+        horizontalLayout->addWidget(tableView);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 20));
+        menuAjouter = new QMenu(menubar);
+        menuAjouter->setObjectName(QString::fromUtf8("menuAjouter"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuAjouter->menuAction());
+        menuAjouter->addAction(actionNouveau_Contact);
+        menuAjouter->addAction(actionNouvelle_Interaction);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionNouveau_Contact->setText(QApplication::translate("MainWindow", "&Nouveau Contact", nullptr));
+        actionNouvelle_Interaction->setText(QApplication::translate("MainWindow", "Nouvelle &Interaction", nullptr));
+        menuAjouter->setTitle(QApplication::translate("MainWindow", "A&jouter", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
